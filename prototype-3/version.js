6c7f4327245c710e4c0b7bc99f371569d40874d1
Илья Савенок <i7ok@yandex.ru>
2026-09-03 19:50:38 +0300

    Remove automatic prototype version bump

--- prototype-3/version.js
+++ prototype-3/version.js
@@ -1,2 +1,2 @@
-/* Единственный номер ревизии; GitHub Actions увеличивает его при push в main. */
-window.PROTOTYPE_VERSION = '3.5';
+/* Единственный номер ревизии; повышается вручную в каждом обычном коммите. */
+window.PROTOTYPE_VERSION = '3.6';
